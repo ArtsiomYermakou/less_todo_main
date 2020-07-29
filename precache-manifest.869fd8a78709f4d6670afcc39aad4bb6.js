c6e244d9dd1c13fc5864b5926b8d16c6ed07bf16
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-07-29 16:57:13 +0300

    Updates

---
+++ precache-manifest.869fd8a78709f4d6670afcc39aad4bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5288ec2cf00aab3428e2c092e251d6e5",
+    "url": "/less_todo_main/index.html"
+  },
+  {
+    "revision": "d14ce1e13b6581a32061",
+    "url": "/less_todo_main/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "82c8b454d7c29b098506",
+    "url": "/less_todo_main/static/js/2.37b5ca8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/less_todo_main/static/js/2.37b5ca8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14ce1e13b6581a32061",
+    "url": "/less_todo_main/static/js/main.9ae067af.chunk.js"
+  },
+  {
+    "revision": "1ecba427d0b364fda15c",
+    "url": "/less_todo_main/static/js/runtime-main.1cb63532.js"
+  }
+]);
