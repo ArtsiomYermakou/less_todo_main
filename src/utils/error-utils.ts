becc8fda87d6650295475410aa8306c0c9570f42
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-10-02 16:14:27 +0300

    stage 15 DONE. error-utils

---
+++ src/utils/error-utils.ts
@@ -0,0 +1,17 @@
+import {ResponseType} from "../api/todolists-api"
+import {Dispatch} from "redux";
+import {setAppErrorAC, setAppStatusAC, setAppErrorActionType, SetAppStatusActionType} from "../app/app-reducer";
+
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<setAppErrorActionType | SetAppStatusActionType>) => {
+    if (data.messages.length) {
+        dispatch(setAppErrorAC(data.messages[0]))
+    } else {
+        dispatch(setAppErrorAC("Some error occurred"))
+    }
+    dispatch(setAppStatusAC("failed"))
+}
+
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<setAppErrorActionType | SetAppStatusActionType>) => {
+    dispatch(setAppErrorAC(error.message ? error.message : "Some error occurred"))
+    dispatch(setAppStatusAC("failed"))
+}
